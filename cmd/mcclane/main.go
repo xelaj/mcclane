2dc5ab995fa418ab82bf1d7b2ebed0267f577d39
ff0000 ff <fmx0209@gmail.com>
2019-12-14 18:09:16 +0300

    wtf im doing

--- cmd/mcclane/main.go
+++ cmd/mcclane/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"mcclane/internal/telegram"
+)
+const botTocken = "CLASSIFED"
 
 func main() {
-	fmt.Println("ACAB")
+
+	tgBot, err := tgbotapi.NewBotAPI(botTocken)
+	if err != nil {
+		fmt.Println("error while connecting to telegram", err)
+
+	}
+	x := telegram.New(tgBot)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60 // TODO: extract
+	updates, err := tgBot.GetUpdatesChan(u)
+	if err != nil {
+		fmt.Println("Error GetUpdatesChan")
+	}
+	exit := make(chan telegram.UserLocation)
+	for update := range updates {
+		loc, err := x.GetLocation(update)
+		if err != nil {
+			continue
+		}
+		exit <- *loc
+		x.Listen(exit)
+	}
+
 }
